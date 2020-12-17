57b0190c848a307eb379c43c854ed699b88f378f
Noeliafloresp <75977547+Noeliafloresp@users.noreply.github.com>
2020-12-17 17:14:42 +0100

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -93,8 +93,7 @@ export default ({ posts = [], preview }) => {
                 {(!post.preview || post.preview.length === 0) &&
                   'No preview available'}
                  {(post.preview || []).map((block, idx) => {
-                    const { value } = block
-                    const { type, properties, id } = value
+                    const { type, properties, id } = block
                     switch (type) {
                       case 'text':
                         textBlock(block, true, `${post.Slug}${idx}`)
